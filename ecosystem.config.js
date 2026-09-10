b43dcd066d38700f4a98392270bcaaa6b90f49ed
minimac <garphy22@163.com>
2026-09-10 14:25:18 +0800

    chore(deploy): add pm2 ecosystem configuration file

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'bugtracer',
+      script: './run.py',
+      cwd: __dirname,
+      interpreter: './.venv/bin/python',
+      exec_mode: 'fork',
+      instances: 1,
+      autorestart: true,
+      max_restarts: 10,
+      restart_delay: 2000,
+      watch: false,
+      env: {
+        PYTHONUNBUFFERED: '1'
+      }
+    }
+  ]
+};
